d5596df770ea6c9adb63b7ce1b2ee089dab12f1f
Chaitanya Kumar <chaitanyak28771@gmail.com>
2023-08-07 19:05:16 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,44 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Appearance and Startup
+(set-scroll-bar-mode nil)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
+
+;; Packages
+(straight-use-package 'doom-themes)
+(straight-use-package 'julia-mode)
+(straight-use-package 'julia-repl)
+(straight-use-package 'eglot-jl)
+(straight-use-package 'ace-window)
+
+
+;; Customize
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-dracula))
+ '(custom-safe-themes
+   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" default))
+ '(eglot-connect-timeout 600))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
